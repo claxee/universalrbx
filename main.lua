f6377b86ebdfb229f9f34cfd7d0f5b36528ceca3
claxee <kokmember169@gmail.com>
2024-05-18 21:04:09 +0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,31 @@
+--THIS IS A REAL SCRIPT ( NO TROLL )
+
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+OrionLib:MakeNotification({
+	Name = "CP UI-Loading...",
+	Content = "Child porn made by kids for kids!",
+	Image = "rbxassetid://17301768250",
+	Time = 10
+})
+wait(0.3)
+OrionLib:MakeNotification({
+	Name = "CP UI-Loading...",
+	Content = "Child porn made by kids for kids!",
+	Image = "rbxassetid://17301768250",
+	Time = 10
+})
+wait(0.3)
+OrionLib:MakeNotification({
+	Name = "CP UI-Loading...",
+	Content = "Child porn made by kids for kids!",
+	Image = "rbxassetid://17301768250",
+	Time = 10
+})
+wait(0.3)
+OrionLib:MakeNotification({
+	Name = "CP UI-Loading...",
+	Content = "Child porn made by kids for kids!",
+	Image = "rbxassetid://17301768250",
+	Time = 10
+})
+
